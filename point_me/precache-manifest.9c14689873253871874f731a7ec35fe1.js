16ceca1ca5e15134293aef124874057890830e83
fudgi <user@users-MacBook-Pro.local>
2019-03-31 18:00:54 +0300

    map added

---
+++ point_me/precache-manifest.9c14689873253871874f731a7ec35fe1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f449bd2787b302e28ac4fb7eaaae21e9",
+    "url": "./static/media/trash.f449bd27.svg"
+  },
+  {
+    "revision": "8086623cc49767eb6ef48a5ae0cf73ed",
+    "url": "./static/media/sidebar.8086623c.svg"
+  },
+  {
+    "revision": "d7e89df62482e9861adc7bd652de772a",
+    "url": "./static/media/direction.d7e89df6.svg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "./static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "17fdf11a377ed4e7f78f",
+    "url": "./static/js/main.578423d6.chunk.js"
+  },
+  {
+    "revision": "083d2d7b0349723f1a08",
+    "url": "./static/js/2.8e775f59.chunk.js"
+  },
+  {
+    "revision": "17fdf11a377ed4e7f78f",
+    "url": "./static/css/main.4d0be7da.chunk.css"
+  },
+  {
+    "revision": "77da94417fabec17f1ff395d049ee40e",
+    "url": "./index.html"
+  }
+];
